e8d71135c36fe34d441143c4b7a5cc2c532fdc94
hoanganhtruc1501 <hoangtruc899@gmail.com>
2026-04-22 23:37:39 +0700

    c:usersAdminwebdoanvat

---
+++ snackstore/apps/orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from . import views
+
+app_name = "orders"
+
+urlpatterns = [
+    path("", views.order_list_view, name="list"),
+    path("create/", views.order_create_view, name="create"),
+    path("checkout/", views.checkout_view, name="checkout"),
+    path("<int:order_id>/", views.order_detail_view, name="detail"),
+    path("orders/", include("orders.urls")),
+]
