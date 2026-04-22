5724b38cbddde88fa5a10b93da8f28ef7ef87620
lecong222006-prog <lecong222006@gmail.com>
2026-04-22 21:22:12 +0700

    Update admin.py

--- snackstore/apps/snacks/admin.py
+++ snackstore/apps/snacks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Snack
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "is_active")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Snack)
+class SnackAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "discount", "stock")
+    list_filter = ("category",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
