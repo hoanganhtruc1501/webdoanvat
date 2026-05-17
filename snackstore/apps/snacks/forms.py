7cab1500c8420a102d46a34eb6f79cb614c4252d
nguyentrung12-siuu <toilatrungg2612@gmail.com>
2026-05-17 22:16:13 +0700

    forms.py

--- snackstore/apps/snacks/forms.py
+++ snackstore/apps/snacks/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import Review
+from .models import HomeComment, Review
 
 
 class CustomUserCreationForm(UserCreationForm):
